d52cda62d17f51c27f47bd57b761bbc62e4daaab
bhavanisarma <bhavani.sarma@gmail.com>
2019-11-24 21:05:35 +0000

    first-test-factorybot

--- Gemfile
+++ Gemfile
@@ -70,3 +70,7 @@ end
 
 gem 'devise'
 gem 'simple_form'
+group :development, :test do
+  gem 'rspec-rails', '~> 3.5'
+  gem "factory_bot_rails"
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'home#index'
+  # root "example#index" - This line added to verify the testing framework works well
 end
