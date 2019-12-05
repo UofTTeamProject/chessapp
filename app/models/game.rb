1c96eaf30a26aa1e1164d765503ca3055e7cf731
dbielza <dbielza@ryerson.ca>
2019-12-05 00:15:28 +0000

    Scope and List

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,11 @@
-
 class Game < ApplicationRecord
+  scope :available, -> {where(white_player_id: nil).or(where(black_player_id: nil))}
+
+  def available_player
+    if white_player_id 
+      User.find(white_player_id)
+    else
+      User.find(black_player_id)
+    end
+  end
 end
--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -1,5 +1,44 @@
 require 'rails_helper'
 
 RSpec.describe Game, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "Game#available" do
+    it "returns a game with a missing black_player_id" do
+      game = Game.new
+      game.white_player_id = 1
+      game.save
+      available_games = Game.available
+      puts available_games.inspect
+      assert available_games.first == game
+    end
+
+    it "returns a game with a missing white_player_id" do
+      game = Game.new
+      game.black_player_id = 1
+      game.save
+      available_games = Game.available
+      assert available_games.first == game
+    end
+
+    it "returns all available games" do
+      game1 = Game.new
+      game2 = Game.new
+      game1.white_player_id = 1
+      game2.black_player_id = 1
+      game1.save
+      game2.save
+      available_games = Game.available
+      assert available_games.find(game1.id)
+      assert available_games.find(game2.id)
+    end
+
+    it "does not return games with a white_player_id and black_player_id" do
+      game = Game.new
+      game.white_player_id = 1
+      game.black_player_id = 1
+      game.save
+      available_games = Game.available
+      assert available_games.first != game
+    end
+  end
 end
+
