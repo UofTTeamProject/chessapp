059c0c2f1af394c195f3e80adbff2b842d2e121a
Sunny <spp1991@gmail.com>
2019-12-09 01:48:18 +0000

    Resolved conflicts

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -1,2 +1,25 @@
 class Piece < ApplicationRecord
+    #main base class
 end
+
+class Pawn < Piece
+end
+
+class Knight < Piece
+end
+
+class Bishop < Piece
+end
+
+class Rook < Piece
+end
+
+class Queen < Piece
+end
+
+class King < Piece
+end
+
+
+
+
