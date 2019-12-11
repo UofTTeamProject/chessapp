9032d1a2a3378e964c4eeddef953e57b1a9ac055
Sunny <spp1991@gmail.com>
2019-12-11 05:53:35 +0000

    commented puts related code

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -35,16 +35,16 @@ class GamesController < ApplicationController
 		if start_x > dest_x; small_x,high_x = dest_x,start_x end
 		if start_y > dest_y; small_y,high_y = dest_y,start_y end
 		
-		puts "small_x:" + small_x.to_s
-		puts "small_y:" + small_y.to_s
-		puts "high_x:" + high_x.to_s
-		puts "high_y:" + high_y.to_s
+		#puts "small_x:" + small_x.to_s
+		#puts "small_y:" + small_y.to_s
+		#puts "high_x:" + high_x.to_s
+		#puts "high_y:" + high_y.to_s
 
 		if small_x == high_x
 			#check for vertical obstruction
-			puts "Horizontal"
+			#puts "Horizontal"
 			for i in small_y..(high_y-1) do
-				puts "i:" + i.to_s
+				#puts "i:" + i.to_s
 				move = current_game.moves.where("current_position = '#{small_x}*#{i+1}'").order('created_at DESC').first
 				if !move.nil?
 					return true
@@ -52,9 +52,9 @@ class GamesController < ApplicationController
 			end	
 		elsif small_y == high_y
 			#check for horizontal obstruction
-			puts "Vertical"
+			#puts "Vertical"
 			for i in small_x..(high_x-1) do
-				puts "i:" + i.to_s
+				#puts "i:" + i.to_s
 				move = current_game.moves.where("current_position = '#{i+1}*#{small_y}'").order('created_at DESC').first
 				if !move.nil?
 					return true
@@ -62,9 +62,9 @@ class GamesController < ApplicationController
 			end
 		elsif (high_x-small_x) == (high_y-small_y)
 			#check for diagonal obstruction
-			puts "Diagonal"
+			#puts "Diagonal"
 			(high_x-small_x-1).abs.times do |i|
-				puts "i:" + i.to_s
+				#puts "i:" + i.to_s
 				move = current_game.moves.where("current_position = '#{i+small_x+1}*#{i+small_y+1}'").order('created_at DESC').first
 				if !move.nil?
 					return true
