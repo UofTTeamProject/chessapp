a2c487d4dd18e6951fc6bd489c8afc8a2e6c13b4
Sunny <spp1991@gmail.com>
2019-12-17 09:23:12 +0000

    updated is_valid and other method to reflect current database strcture

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -1,6 +1,13 @@
 class Piece < ApplicationRecord
     #main base class
     has_many :moves
+    validate :is_validmove? , on: :update
+
+    def is_validmove?
+        true
+    end
+
+
 end
 
 
--- spec/models/king_spec.rb
+++ spec/models/king_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe King, type: :model do
-  
+    describe "King#update" do
+        it "successfully updates record when King is moved to correct location" do
+            #dummy game ,user1 ,user2 and King piece creation
+            user1 = FactoryBot.create(:user)
+            user2 = FactoryBot.create(:user)
+            game = Game.create(white_player_id:user1.id,black_player_id:user2.id,status:-1)
+            piece = Piece.create(piece_type:"King",piece_color:"white",type:"King",x_position:1,y_position:1)
+            move = Piece.find(piece.id).update_attributes(x_position:2,y_position:2)
+            expect(move).to eq(true)
+        end
+        it "returns a error when a king piece is  moved to wrong/invalid location" do
+            #dummy game ,user1 ,user2 and King piece creation
+            user1 = FactoryBot.create(:user)
+            user2 = FactoryBot.create(:user)
+            game = Game.create(white_player_id:user1.id,black_player_id:user2.id,status:-1)
+            piece = Piece.create(piece_type:"King",piece_color:"white",type:"King",x_position:1,y_position:1)
+
+            move = Piece.find(piece.id).update_attributes(x_position:2,y_position:3)
+            expect(move).to eq(false)
+        end
+    end
 end
