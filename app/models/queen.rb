93584c959a8de8a4ae177240d5560f16ae105626
bhavanisarma <bhavani.sarma@gmail.com>
2019-12-24 21:44:44 +0000

    queen-validmove

--- app/models/queen.rb
+++ app/models/queen.rb
@@ -1,2 +1,16 @@
 class Queen < Piece
+
+    validate :is_validmove?, on: :update
+    def is_validmove?
+        dest_x,dest_y = self.x_position,self.y_position
+        previous_piece_move = Piece.find(self.id)
+        start_x,start_y = previous_piece_move.x_position,previous_piece_move.y_position
+        if ((start_x == dest_x || start_y == dest_y) || ((start_x.to_i - dest_x.to_i).abs == (start_y.to_i - dest_y.to_i).abs))
+            return true
+        else
+            errors[:x_position] << "Not a valid Move"
+            return false
+        end
+    end
 end
+
--- spec/models/queen_spec.rb
+++ spec/models/queen_spec.rb
@@ -1,5 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe Queen, type: :model do
+	describe "Queen#update" do
+        it "successfully updates record when Queen is moved to correct location" do
+            #dummy game ,user1 ,user2 and Queen piece creation
+            user1 = FactoryBot.create(:user)
+            user2 = FactoryBot.create(:user)
+            game = Game.create(white_player_id:user1.id,black_player_id:user2.id,status:-1)
+            piece = Piece.create(piece_type:"Queen",piece_color:"white",type:"Queen",x_position:1,y_position:4)
+            move = Piece.find(piece.id).update_attributes(x_position:5,y_position:4) #for vertical movement
+            expect(move).to eq(true)
+            #for horizontal movement
+            piece = Piece.create(piece_type:"Queen",piece_color:"white",type:"Queen",x_position:1,y_position:4)
+            move = Piece.find(piece.id).update_attributes(x_position:1,y_position:6)
+            expect(move).to eq(true)
+            #for diagonal movement
+            piece = Piece.create(piece_type:"Queen",piece_color:"white",type:"Queen",x_position:1,y_position:4)
+            move = Piece.find(piece.id).update_attributes(x_position:3,y_position:6)
+            expect(move).to eq(true)
+        end
+        
+        it "returns a error when a Queen piece is  moved to wrong/invalid location" do
+            #dummy game ,user1 ,user2 and Bishop piece creation
+            user1 = FactoryBot.create(:user)
+            user2 = FactoryBot.create(:user)
+            game = Game.create(white_player_id:user1.id,black_player_id:user2.id,status:-1)
+            piece = Piece.create(piece_type:"Queen",piece_color:"white",type:"Queen",x_position:2,y_position:2)
+            move = Piece.find(piece.id).update_attributes(x_position:4,y_position:1)
+            expect(move).to eq(false)
+        end
+    end
   
 end
